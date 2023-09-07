86888e0ebb937975dc0116ac6d8f2154dceb4b30
AdityaRitpurkar <adityaritpurkar@gmail.com>
2023-09-07 16:24:34 +0530

    model classes created

--- src/main/java/com/bootcoding/project/model/OrderMenuItem.java
+++ src/main/java/com/bootcoding/project/model/OrderMenuItem.java
@@ -6,4 +6,27 @@ public class OrderMenuItem {
     String deliveryAddress;
     long contact;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public long getContact() {
+        return contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
 }
